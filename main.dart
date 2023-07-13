6f235bfa1e2dda9992e153406b5fc092160587f8
Harishthehacker <65214091+Harishthehacker@users.noreply.github.com>
2023-07-13 23:42:26 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,171 @@
+﻿import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                color: Colors.purple,
+                padding: const EdgeInsets.only(left: 5.0, top: 10),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sign In to Your',
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                    Text(
+                      'Account',
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Sign In to Your Account',
+                      style: TextStyle(fontSize: 12, color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          color: Colors.purple,
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text('Email',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: TextButton(
+                            onPressed: () {}, child: const Text('Phone')),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                ),
+              ),
+              const SizedBox(height: 35),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Row(
+                children: [
+                  Expanded(child: Divider(indent: 16)),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                    child:
+                        Text('or sign up with', style: TextStyle(fontSize: 18)),
+                  ),
+                  Expanded(child: Divider(endIndent: 16)),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://www.gstatic.com/images/branding/product/1x/gsa_512dp.png',
+                    width: 32,
+                    height: 32,
+                  ),
+                  const SizedBox(width: 8),
+                  const Text('Google', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+              const SizedBox(height: 40),
+              const Text.rich(TextSpan(
+                  text: 'Not registered yet? ',
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                      text: 'Create Account',
+                      style: TextStyle(color: Colors.purple),
+                    )
+                  ])),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
